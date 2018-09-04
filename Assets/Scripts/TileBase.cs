4134b7b62d5600a2f4846c1d97bf47feb1aa40d0
TheStudent0 <siemonwerk@hotmail.com>
2018-09-04 15:37:42 +0200

    Added some basics
    Added Scene
    Added Maps
    Added BaseClass

---
+++ Assets/Scripts/TileBase.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileBase : MonoBehaviour {
+    protected float HP;
+    protected bool IsDestroyed;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void TakeDamage(float damage)
+    {
+        HP -= damage;
+        IsDestroyed = HP <= 0.0f;
+    }
+
+    bool GetIsDestroyed()
+    {
+        return IsDestroyed;
+    }
+
+    float GetHP()
+    {
+        return HP;
+    }
+}
