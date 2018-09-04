d4034ebfec537faebf99b770897a7c9f86c0cc5a
TheStudent0 <siemonwerk@hotmail.com>
2018-09-04 15:55:49 +0200

    Updated folder structure + more tiles

---
+++ Assets/Scripts/Tiles/RoomTile.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingTile : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
