1861f57909a36837fae43f0d41cc49669ae95fe9
JordyV500 <verbruggenjordy@hotmail.com>
2018-09-06 11:04:44 +0200

    Roomtile rename

--- Assets/Scripts/Tiles/RoomTile.cs
+++ Assets/Scripts/Tiles/RoomTile.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BuildingTile : TileBase {
+public class RoomTile : TileBase {
 	// Use this for initialization
-	void Start ()
+	protected override void Start ()
     {
-		
+        base.Start();
 	}
 	
 	// Update is called once per frame
